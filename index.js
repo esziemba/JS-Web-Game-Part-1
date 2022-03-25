218ae47f2348a85f92e36a34743c01ba6a8fe522
esziemba <97561230+esziemba@users.noreply.github.com>
2022-03-25 11:55:08 -0500

    Completed Web Game Part 1

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Create a function to add objects to the screen
+
+function newImage(imgSrc, left, bottom){
+    let object = document.createElement('img')
+    object.src = imgSrc
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+// Item Functions to add items with dblclick listener to remove item 
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+// Function Calls for Images and Items 
+
+newImage('assets/green-character.gif', 50, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+newItem('assets/green-character.gif', 50, 100)
